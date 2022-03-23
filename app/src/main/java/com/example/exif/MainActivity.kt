53cd9cfbb3e687d5095cf8b24d56a2a126cd9108
Yao Zhang <alanzhangyao@outlook.com>
2022-03-23 17:50:28 -0400

    init

---
+++ app/src/main/java/com/example/exif/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.exif
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.media.ExifInterface
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var text : TextView
+    var checksum = 0
+    private fun getPhotoFile(){
+        val intent = Intent(Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        startForResult.launch(intent)
+    }
+    fun getRealPathFromURI(contentUri: Uri?): String? {
+
+        // can post image
+        val proj = arrayOf(MediaStore.Images.Media.DATA)
+        var uri = contentUri
+        val cursor: Cursor = managedQuery(
+            uri,
+            proj,  // Which columns to return
+            null,  // WHERE clause; which rows to return (all rows)
+            null,  // WHERE clause selection arguments (none)
+            null
+        ) // Order-by clause (ascending by name)
+        val column_index: Int = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        cursor.moveToFirst()
+        return cursor.getString(column_index)
+    }//https://stackoverflow.com/questions/18590514/loading-all-the-images-from-gallery-into-the-application-in-android
+    val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val intent = result.data?.clipData
+            val count = intent?.itemCount
+            val sb = StringBuilder()
+            for (i in 0..count!!-1){
+                val path = getRealPathFromURI(intent.getItemAt(i)?.uri)
+                System.out.println(path)
+                val file = File(path!!)
+                val exif = ExifInterface(file)
+                val array = FloatArray(2)
+                exif.getLatLong(array)
+                System.out.println(array[0])
+                val calendar: Calendar = Calendar.getInstance()
+                calendar.setTimeInMillis(exif.dateTime)
+                val formatter = SimpleDateFormat("MM/dd/yyyy hh:mm:ss.SSS")
+
+                sb.append("Photo $i: lat: " + array[0] + " long: " + array[1]  + " time: " + formatter.format(calendar.getTime()) + "\n")
+            }
+            text.setText(sb.toString())
+
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        text = findViewById(R.id.text)
+        checkPermission(Manifest.permission.ACCESS_MEDIA_LOCATION, 0)
+        checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE, 1)
+        if (checksum ==2) getPhotoFile()
+        else{
+            text.setText("Permissions are needed to use this app. Please reload this app again.")
+        }
+    }
+
+    private fun checkPermission(permission: String, requestCode: Int) {
+        System.out.println(permission)
+        if (ContextCompat.checkSelfPermission(this@MainActivity, permission) == PackageManager.PERMISSION_DENIED) {
+            // Requesting the permission
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+        } else {
+            checksum += 1
+        }
+    }
+}
