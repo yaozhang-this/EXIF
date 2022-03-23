590d88e90a843904a7ed9d30b505ca86926624ab
Yao Zhang <alanzhangyao@outlook.com>
2022-03-23 19:58:35 -0400

    it works!

--- app/src/main/java/com/example/exif/MainActivity.kt
+++ app/src/main/java/com/example/exif/MainActivity.kt
@@ -19,19 +19,14 @@ import java.io.File
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var text : TextView
     var checksum = 0
-    private fun getPhotoFile(){
-        val intent = Intent(Intent.ACTION_PICK,
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-        startForResult.launch(intent)
-    }
-    fun getRealPathFromURI(contentUri: Uri?): String? {
-
+    fun getRealPathFromURI(contentUri: Uri?): ArrayList<String> {
+        val galleryURLs = ArrayList<String>()
         // can post image
         val proj = arrayOf(MediaStore.Images.Media.DATA)
         var uri = contentUri
@@ -42,31 +37,31 @@ class MainActivity : AppCompatActivity() {
             null,  // WHERE clause selection arguments (none)
             MediaStore.Images.Media.DEFAULT_SORT_ORDER
         ) // Order-by clause (ascending by name)
-        val column_index: Int = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-        cursor.moveToFirst()
-        return cursor.getString(column_index)
-    }//https://stackoverflow.com/questions/18590514/loading-all-the-images-from-gallery-into-the-application-in-android
-    val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-        if (result.resultCode == Activity.RESULT_OK) {
-            val intent = result.data?.clipData
-            val count = intent?.itemCount
-            val sb = StringBuilder()
-            for (i in 0..count!!-1){
-                val path = getRealPathFromURI(intent.getItemAt(i)?.uri)
-                System.out.println(path)
-                val file = File(path!!)
-                val exif = ExifInterface(file)
-                val array = FloatArray(2)
-                exif.getLatLong(array)
-                //default value is 0/0
-
-                val formatterDate = SimpleDateFormat("MM/dd/yyyy", Locale.US)
-                val formatterTime = SimpleDateFormat("hh:mm:ss", Locale.US)
-                sb.append("Photo $i: lat: " + array[0] + " long: " + array[1]  + " date: " + formatterDate.format(exif.dateTimeOriginal) + " time: " + formatterTime.format(exif.dateTimeOriginal) + "\n")
-            }
-            text.setText(sb.toString())
-
+        for (i in 0..cursor.count-1)
+        {
+            cursor.moveToPosition(i)
+            val dataColumnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA)
+            galleryURLs.add(cursor.getString(dataColumnIndex))
         }
+        return galleryURLs
+    }//https://stackoverflow.com/questions/18590514/loading-all-the-images-from-gallery-into-the-application-in-android
+
+    fun readAll(arrayList : ArrayList<String>){
+        val sb = StringBuilder()
+        for (i in 0..arrayList.size -1){
+            val path = arrayList[i]
+            System.out.println(path)
+            val file = File(path)
+            val exif = ExifInterface(file)
+            val array = FloatArray(2)
+            exif.getLatLong(array)
+            //default value is 0/0
+
+            val formatterDate = SimpleDateFormat("MM/dd/yyyy", Locale.US)
+            val formatterTime = SimpleDateFormat("hh:mm:ss", Locale.US)
+            sb.append("Photo $i: lat: " + array[0] + " long: " + array[1]  + " date: " + formatterDate.format(exif.dateTimeOriginal) + " time: " + formatterTime.format(exif.dateTimeOriginal) + "\n")
+        }
+        text.setText(sb.toString())
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -75,7 +70,10 @@ class MainActivity : AppCompatActivity() {
         text = findViewById(R.id.text)
         checkPermission(Manifest.permission.ACCESS_MEDIA_LOCATION, 0)
         checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE, 1)
-        if (checksum ==2) getPhotoFile()
+        if (checksum ==2) {
+            readAll(getRealPathFromURI(MediaStore.Images.Media.EXTERNAL_CONTENT_URI))
+
+        }
         else{
             text.setText("Permissions are needed to use this app. Please reload this app again.")
         }
